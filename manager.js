c24fbca8955927422c929652b0679bd49d011521
foomet <91850182+foomet@users.noreply.github.com>
2023-04-16 18:00:57 -0600

    Management side for Part 3

--- manager.js
+++ manager.js
@@ -3,13 +3,32 @@ window.onload = function(){
     const deleteButtons = document.getElementsByClassName("deleteButton");
     const addFlavorButton = document.getElementById('addFlavor');
 
-    // addFlavorButton.addEventListener("click", function() {
+    const refundOrderButtons = document.getElementsByClassName("refundOrder");
+    const viewOrderButtons = document.getElementsByClassName("viewOrder");
+
+    // Add Icecream
+    addFlavorButton.addEventListener("click", function() {
+
+        newIcecreamName = prompt("What should the name be? ");
+        if(newIcecreamName === null){
+            return;
+        }
+        else{
+            newIcecreamPrice = prompt("What should the price be?");
+            if(newIcecreamPrice === null){
+                return;
+            }
+            else{
+                // All new icecreams default to the image of chocolate icecream.
+                localStorage.setItem(newIcecreamName, JSON.stringify([newIcecreamPrice, "./images/chocolate.jfif", true]))
+                window.location.reload();
+            }
+        }
+
+    })
 
 
-
-    // })
-
-
+    // Edit Icecream:
     for(let i = 0; i < editButtons.length; i++){
         editButtons[i].addEventListener("click", function() {
 
@@ -41,8 +60,7 @@ window.onload = function(){
         });
     }
 
-
-
+    // Delete Icecream: 
     // Note that we can't delete the icecream from local storage.
     // If that happens, then the icecream will be regenerated in manager.html
     for(let i = 0; i < deleteButtons.length; i++){
@@ -71,6 +89,70 @@ window.onload = function(){
                 // This reload resolves a bug.
                 window.location.reload();
             }
+        })
+    }
+
+    // View Order Buttons
+    for(let i = 0; i < viewOrderButtons.length; i++){
+        viewOrderButtons[i].addEventListener("click", function(){
+
+            specificOrder = viewOrderButtons[i].id;
+
+            lastIndex = specificOrder.lastIndexOf(" ");
+            specificOrder = specificOrder.substring(0, lastIndex);
+
+            specificOrder = specificOrder.toString();
+            
+            fetch('./orders.json')
+                .then((response) => response.json())
+                .then((orderElements) => {
+
+                    for(let j=0; j<orderElements.length; j++){
+                        if(specificOrder == orderElements[j].id){
+                            alert(orderElements[j].items);
+                        }
+                        else{
+
+                        }
+                    }
+
+                });
+
+        })
+    }
+
+    // Refund Order Buttons
+    for(let i = 0; i < refundOrderButtons.length; i++){
+        refundOrderButtons[i].addEventListener("click", function(){
+
+            specificOrder = viewOrderButtons[i].id;
+
+            lastIndex = specificOrder.lastIndexOf(" ");
+            specificOrder = specificOrder.substring(0, lastIndex);
+
+            specificOrder = specificOrder.toString();
+            
+            fetch('./orders.json')
+                .then((response) => response.json())
+                .then((orderElements) => {
+
+                    for(let j=0; j<orderElements.length; j++){
+                        if(specificOrder == orderElements[j].id){
+                            confirmation = confirm("Are you sure you want to refund this order?");
+                            if(confirmation == false){
+                                return;
+                            }
+                            else{
+                                alert("Order refunded.");
+                            }
+                        }
+                        else{
+
+                        }
+                    }
+
+                });
+
         })
     }
 }
