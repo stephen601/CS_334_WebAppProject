dc7aeb5bbcfc164008236bf840ca893622f223df
Stephen601 <stevewash256@gmai.com>
2023-05-06 16:38:38 -0600

    SQLAlchemy update

--- Part 4/API/app.py
+++ Part 4/API/app.py
@@ -1,246 +1,220 @@
-from flask import Flask, make_response, jsonify, request
-import json
-import sqlite3
-
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ice_cream.sqlite'
+db = SQLAlchemy(app)
 
-def db_connection():
-    conn = None
-    try:
-        conn = sqlite3.connect('ice_cream.sqlite')
-    except sqlite3.error as e:
-        print(e)
-    return conn
+class IceCream(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    image = db.Column(db.String(100))
+
+class Usernames(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    is_admin = db.Column(db.Integer, default=0)
+
+class Orders(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ice_cream_id = db.Column(db.Integer, db.ForeignKey('ice_cream.id'), nullable=False)
+    username_id = db.Column(db.Integer, db.ForeignKey('usernames.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    total_price = db.Column(db.Float, nullable=False)
+    order_date = db.Column(db.DateTime, nullable=False)
+    ice_cream = db.relationship('IceCream', backref=db.backref('orders', lazy=True))
+    username = db.relationship('Usernames', backref=db.backref('orders', lazy=True))
 
 @app.route("/ice_cream", methods=["GET", "POST"])
 def ice_cream():
-    conn = db_connection()
-    cursor = conn.cursor()
-
     if request.method=="GET":
-        cursor.execute("SELECT * FROM ice_cream")
-
-        ice_cream = [
-            dict(id=row[0], name=row[1], price = row[2], image = row[3])
-            for row in cursor.fetchall()
-        ]
-        if ice_cream is not None:
-            return jsonify(ice_cream)
+        ice_cream = IceCream.query.all()
+        return jsonify([{"id": i.id, "name": i.name, "price": i.price, "image": i.image} for i in ice_cream])
 
     if request.method == "POST":
         new_name = request.form["name"]
         new_price = request.form["price"]
         new_image = request.form["image"]
-
-        sql = """INSERT INTO ice_cream (name, price, image)
-                 VALUES (?,?,?)"""
-
-        cursor = cursor.execute(sql, (new_name, new_price))
-        conn.commit()
-        return f"Ice creame with the id: {cursor.lastrowid} created sussessfully", 201
+        new_ice_cream = IceCream(name=new_name, price=new_price, image=new_image)
+        db.session.add(new_ice_cream)
+        db.session.commit()
+        return f"Ice cream with id: {new_ice_cream.id} created successfully", 201
 
 @app.route("/ice_cream/<int:id>", methods=["GET", "PUT", "DELETE"])
 def single_ice_cream(id):
-    conn = db_connection()
-    cursor = conn.cursor()
-    ice_cream = None
+    ice_cream = IceCream.query.get(id)
     if request.method == "GET":
-        cursor.execute("SELECT * FROM ice_cream WHERE id=?", (id,))
-        ice_cream = [
-            dict(ice_cream_id=row[0], name=row[1], price=row[2], image=row[3])
-            for row in cursor.fetchall()
-        ]
-
         if ice_cream:
-            return jsonify(ice_cream)
+            return jsonify({"id": ice_cream.id, "name": ice_cream.name, "price": ice_cream.price, "image": ice_cream.image})
         else:
-            return f"No orders found for user with id: {id}", 404
-
+            return f"No ice cream found for id: {id}", 404
 
     if request.method == "PUT":
-        sql = """UPDATE ice_cream
-                SET name=?,
-                    price=?,
-                    image=?
-                WHERE id=? """
-
-        name = request.form["name"]
-        price = request.form["price"]
-        image = request.form["image"]
-        updated_ice_cream = {
-            "id": id,
-            "name": name,
-            "price": price,
-            "image": image
-        }
-        conn.execute(sql, (name, price, image, id))
-        conn.commit()
-        return jsonify(updated_ice_cream)
+        if ice_cream:
+            ice_cream.name = request.form["name"]
+            ice_cream.price = request.form["price"]
+            ice_cream.image = request.form["image"]
+            db.session.commit()
+            return jsonify({"id": ice_cream.id, "name": ice_cream.name, "price": ice_cream.price, "image": ice_cream.image})
+        else:
+            return f"No ice cream found for id: {id}", 404
 
     if request.method == "DELETE":
-        sql = """ DELETE FROM ice_cream WHERE id=? """
-        conn.execute(sql, (id,))
-        conn.commit()
-        return "The ice cream with id: {} has been deleted.".format(id), 200
-    
-@app.route("/users", methods=["GET", "POST"])
-def users():
-    conn = db_connection()
-    cursor = conn.cursor()
+        if ice_cream:
+            db.session.delete(ice_cream)
+            db.session.commit()
+            return f"Ice cream with id: {id} deleted successfully", 200
+        else:
+            return f"No ice cream found for id: {id}", 404
 
+@app.route("/user", methods=["GET", "POST"])
+def user():
     if request.method == "GET":
-        cursor.execute("SELECT * FROM usernames")
-        users = [
-            dict(id=row[0], username=row[1], password=row[2], is_admin=row[3])
-            for row in cursor.fetchall()
-        ]
-        if users is not None:
-            return jsonify(users)
+        users = Usernames.query.all()
+        return jsonify([{"id": u.id, "username": u.username, "is_admin": u.is_admin} for u in users])
 
     if request.method == "POST":
         new_username = request.form["username"]
         new_password = request.form["password"]
-        is_admin = request.form.get("is_admin", default=0, type=int)
+        new_is_admin = request.form.get("is_admin", False)
+        new_user = Usernames(username=new_username, password=new_password, is_admin=new_is_admin)
+        db.session.add(new_user)
+        db.session.commit()
+        return f"User with id: {new_user.id} created successfully", 201
 
-        sql = """INSERT INTO usernames (username, password, is_admin)
-                 VALUES (?,?,?)"""
-
-        cursor = cursor.execute(sql, (new_username, new_password, is_admin))
-        conn.commit()
-        return f"User with the id: {cursor.lastrowid} created successfully", 201
-
-@app.route("/users/<int:id>", methods=["GET", "PUT", "DELETE"])
+@app.route("/user/<int:id>", methods=["GET", "PUT", "DELETE"])
 def single_user(id):
-    conn = db_connection()
-    cursor = conn.cursor()
-    user = None
+    user = Usernames.query.get(id)
     if request.method == "GET":
-        cursor.execute("SELECT * FROM usernames WHERE id=?", (id,))
-        users = [
-            dict(user_id=row[0], username=row[1], password=row[2], is_admin=row[3])
-            for row in cursor.fetchall()
-        ]
-
-        if users:
-            return jsonify(users)
+        if user:
+            return jsonify({"id": user.id, "username": user.username, "is_admin": user.is_admin})
         else:
-            return f"No users found for user with id: {id}", 404
-
+            return f"No user found for id: {id}", 404
 
     if request.method == "PUT":
-        sql = """UPDATE usernames
-                SET username=?,
-                    password=?,
-                    is_admin=?
-                WHERE id=? """
-
-        username = request.form["username"]
-        password = request.form["password"]
-        is_admin = request.form.get("is_admin", default=0, type=int)
-        updated_user = {
-            "id": id,
-            "username": username,
-            "password": password,
-            "is_admin": is_admin
-        }
-        conn.execute(sql, (username, password, is_admin, id))
-        conn.commit()
-        return jsonify(updated_user)
+        if user:
+            user.username = request.form["username"]
+            user.password = request.form["password"]
+            user.is_admin = request.form.get("is_admin", user.is_admin)
+            db.session.commit()
+            return jsonify({"id": user.id, "username": user.username, "is_admin": user.is_admin})
+        else:
+            return f"No user found for id: {id}", 404
 
     if request.method == "DELETE":
-        sql = """ DELETE FROM usernames WHERE id=? """
-        conn.execute(sql, (id,))
-        conn.commit()
-        return "User with id: {} has been deleted.".format(id), 200
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+            return f"User with id: {id} deleted successfully", 200
+        else:
+            return f"No user found for id: {id}", 404
 
-@app.route("/orders", methods=["GET", "POST"])
-def orders():
-    conn = db_connection()
-    cursor = conn.cursor()
-
+@app.route("/order", methods=["GET", "POST"])
+def order():
     if request.method == "GET":
-        cursor.execute("SELECT * FROM orders")
-        orders = [
-            dict(id=row[0], user_id=row[1], ice_cream_id=row[2], quantity=row[3])
-            for row in cursor.fetchall()
-        ]
-        if orders is not None:
-            return jsonify(orders)
+        orders = Orders.query.all()
+        return jsonify([{"id": o.id, "username_id": o.username_id, "ice_cream_id": o.ice_cream_id, "quantity": o.quantity, "total_price" : o.total_price, "order_date" : o.order_date} for o in orders])
 
     if request.method == "POST":
-        user_id = request.form["user_id"]
+        username_id = request.form["username_id"]
         ice_cream_id = request.form["ice_cream_id"]
         quantity = request.form["quantity"]
         total_price = request.form["total_price"]
         order_date = request.form["order_date"]
-
-        sql = """INSERT INTO orders (username_id, ice_cream_id, quantity, total_price, order_date)
-                 VALUES (?,?,?,?,?)"""
-
-        cursor = cursor.execute(sql, (user_id, ice_cream_id, quantity, total_price, order_date))
-        conn.commit()
-        return f"Order with id: {cursor.lastrowid} created successfully", 201
-
+        new_order = Orders(username_id=username_id, ice_cream_id=ice_cream_id, quantity=quantity,total_price=total_price, order_date=datetime.strptime(order_date, '%a, %d %b %Y %H:%M:%S %Z'))
+        db.session.add(new_order)
+        db.session.commit()
+        return f"Order with id: {new_order.id} created successfully", 201
+    
 @app.route("/orders/<int:id>", methods=["GET", "PUT", "DELETE"])
 def single_order(id):
-    conn = db_connection()
-    cursor = conn.cursor()
-    order = None
+    order = Orders.query.get(id)
     if request.method == "GET":
-        cursor.execute("SELECT * FROM orders WHERE id=?", (id,))
-        orders = [
-            dict(order_id=row[0], ice_cream_id=row[1], user_id=row[2], quantity=row[3], total_price=row[4], order_date=row[5])
-            for row in cursor.fetchall()
-        ]
-
-        if orders:
-            return jsonify(orders)
+        if order:
+            return jsonify({
+                "id": order.id, 
+                "ice_cream_id": order.ice_cream_id, 
+                "username_id": order.username_id, 
+                "quantity": order.quantity, 
+                "total_price": order.total_price, 
+                "order_date": order.order_date,
+                "ice_cream": {
+                    "id": order.ice_cream.id,
+                    "name": order.ice_cream.name,
+                    "price": order.ice_cream.price,
+                    "image": order.ice_cream.image
+                },
+                "username": {
+                    "id": order.username.id,
+                    "username": order.username.username,
+                    "is_admin": bool(order.username.is_admin)
+                }
+            })
         else:
-            return f"No orders found for user with id: {id}", 404
+            return f"No order found for id: {id}", 404
 
     if request.method == "PUT":
-        sql = """UPDATE orders
-                SET user_id=?,
-                    ice_cream_id=?,
-                    quantity=?
-                WHERE id=? """
+        if order:
+            order.ice_cream_id = request.form["ice_cream_id"]
+            order.username_id = request.form["username_id"]
+            order.quantity = request.form["quantity"]
+            order.total_price = request.form["total_price"]
+            order.order_date = datetime.strptime(request.form["order_date"], '%a, %d %b %Y %H:%M:%S %Z')
 
-        user_id = request.form["user_id"]
-        ice_cream_id = request.form["ice_cream_id"]
-        quantity = request.form["quantity"]
-        updated_order = {
-            "id": id,
-            "user_id": user_id,
-            "ice_cream_id": ice_cream_id,
-            "quantity": quantity
-        }
-        conn.execute(sql, (user_id, ice_cream_id, quantity, id))
-        conn.commit()
-        return jsonify(updated_order)
+            db.session.commit()
+            return jsonify({
+                "id": order.id, 
+                "ice_cream_id": order.ice_cream_id, 
+                "username_id": order.username_id, 
+                "quantity": order.quantity, 
+                "total_price": order.total_price, 
+                "order_date": order.order_date,
+                "ice_cream": {
+                    "id": order.ice_cream.id,
+                    "name": order.ice_cream.name,
+                    "price": order.ice_cream.price,
+                    "image": order.ice_cream.image
+                },
+                "username": {
+                    "id": order.username.id,
+                    "username": order.username.username,
+                    "is_admin": bool(order.username.is_admin)
+                }
+            })
+        else:
+            return f"No order found for id: {id}", 404
 
     if request.method == "DELETE":
-        sql = """ DELETE FROM orders WHERE id=? """
-        conn.execute(sql, (id,))
-        conn.commit()
-        return "Order with id: {} has been deleted.".format(id), 200
+        if order:
+            db.session.delete(order)
+            db.session.commit()
+            return f"Order with id: {id} deleted successfully", 200
+        else:
+            return f"No order found for id: {id}", 404
 
 @app.route("/orders/user/<int:username_id>", methods=["GET"])
 def orders_by_user_id(username_id):
-    conn = db_connection()
-    cursor = conn.cursor()
+    orders = Orders.query.filter_by(username_id=username_id).all()
+    orders_list = []
 
-    cursor.execute("SELECT * FROM orders WHERE username_id=?", (username_id,))
-    orders = [
-        dict(order_id=row[0], ice_cream_id=row[1], user_id=row[2], quantity=row[3], total_price=row[4], order_date=row[5])
-        for row in cursor.fetchall()
-    ]
+    for order in orders:
+        order_dict = {
+            "order_id": order.id,
+            "ice_cream_id": order.ice_cream_id,
+            "user_id": order.username_id,
+            "quantity": order.quantity,
+            "total_price": order.total_price,
+            "order_date": order.order_date,
+        }
+        orders_list.append(order_dict)
 
-    if orders:
-        return jsonify(orders)
+    if orders_list:
+        return jsonify(orders_list)
     else:
         return f"No orders found for user with id: {username_id}", 404
-
 
 if __name__ == '__main__':
     app.run(debug=True)
